4b1ddd6f806b1a04340d2d0ecbf9fa554f2b72a9
hugolu <printk@gmail.com>
2016-09-03 05:03:49 +0200

    patch clusteringApp

--- machine-learning-with-spark/src/ex-7/clusteringApp.scala
+++ machine-learning-with-spark/src/ex-7/clusteringApp.scala
@@ -33,13 +33,16 @@ object clusteringApp {
     val rawRatings = rawData.map(_.split("\t").take(3))
     val ratings = rawRatings.map{ case Array(user, movie, rating) => Rating(user.toInt, movie.toInt, rating.toDouble) }
     ratings.cache
+
     val alsModel = ALS.train(ratings, 50, 10, 0.1)
 
     val movieFactors = alsModel.productFeatures.map{ case (id, factor) => (id, Vectors.dense(factor)) }
     val movieVectors = movieFactors.map(_._2)
+    movieVectors.cache
 
     val userFactors = alsModel.userFeatures.map{ case (id, factor) => (id, Vectors.dense(factor)) }
     val userVectors = userFactors.map(_._2)
+    userVectors.cache
 
     // Normalization
     val movieMatrix = new RowMatrix(movieVectors)
@@ -58,9 +61,11 @@ object clusteringApp {
     val numIterations = 10
     val numRuns = 3
 
-    val movieClusterModel = KMeans.train(movieVectors, numClusters, numIterations, numRuns)
-    val movieClusterModelConverged = KMeans.train(movieVectors, numClusters, numIterations, 100)
-    val userClusterModel = KMeans.train(userVectors, numClusters, numIterations, numRuns)
+    val movieClusterModel = KMeans.train(movieVectors, numClusters, numIterations, numRuns, "k-means||", 42)
+    val movieClusterModelConverged = KMeans.train(movieVectors, numClusters, 100)
+
+    val userClusterModel = KMeans.train(userVectors, numClusters, numIterations, numRuns, "k-means||", 42)
+    val userClusterModelConverged = KMeans.train(userVectors, numClusters, 100)
 
     // Making predictions using a clustering model
     val movieCluster = movieClusterModel.predict(movieVectors.first)
@@ -82,10 +87,9 @@ object clusteringApp {
     val clusterAssignments = movieAssigned.groupBy{ case (id, title, genres, cluster, dist) => cluster }.collectAsMap
 
     for ( (k, v) <- clusterAssignments.toSeq.sortBy(_._1)) {
-      println(s"Cluster $k: ")
+      println(s"\nCluster $k: ")
       val m = v.toSeq.sortBy(_._5)
       println(m.take(10).map{ case (_, title, genres, _, d) => (title, genres, d) }.mkString("\n"))
-      println("====\n")
     }
 
     // Evaluating the performance of clustering models
@@ -95,18 +99,16 @@ object clusteringApp {
     println("WCSS for users: " + userCost)
 
     // Selecting K through cross-validation
-    val trainTestSplitMovies = movieVectors.randomSplit(Array(0.6, 0.4), 123)
-    val trainMovies = trainTestSplitMovies(0)
-    val testMovies = trainTestSplitMovies(1)
+    val Array(trainMovies, testMovies) = movieVectors.randomSplit(Array(0.6, 0.4), 123)
+    trainMovies.cache; testMovies.cache
     val costsMovies = Seq(2, 3, 4, 5, 10, 20).map{ k =>
       (k, KMeans.train(trainMovies, numIterations, k, numRuns).computeCost(testMovies))
     }
     println("Movie clustering cross-validation: ")
     costsMovies.foreach{ case (k, cost) => println(f"WCSS for K=$k id $cost%2.2f") }
 
-    val trainTestSplitUsers = userVectors.randomSplit(Array(0.6, 0.4), 123)
-    val trainUsers = trainTestSplitUsers(0)
-    val testUsers = trainTestSplitUsers(1)
+    val Array(trainUsers, testUsers) = userVectors.randomSplit(Array(0.6, 0.4), 123)
+    trainUsers.cache; testUsers.cache
     val costsUsers = Seq(2, 3, 4, 5, 10, 20).map{ k =>
       (k, KMeans.train(trainUsers, numIterations, k, numRuns).computeCost(testUsers))
     }
