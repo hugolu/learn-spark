4aa9a89665e13eb9b230bcb3689e464f0ae124ef
hugolu <printk@gmail.com>
2016-09-28 10:47:01 +0200

    add an example of network word count

---
+++ spark-streaming/quick-example/NetworkWordCount.scala
@@ -0,0 +1,29 @@
+//package org.apache.spark.examples.streaming
+
+import org.apache.spark.SparkConf
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+object NetworkWordCount {
+  def main(args: Array[String]) {
+
+    if (args.length < 2) {
+      System.err.println("Usage: NetworkWordCount <hostname> <port>")
+      System.exit(1)
+    }
+
+    val conf = new SparkConf().setAppName("NetworkWordCount").setMaster("local[2]")
+    val ssc = new StreamingContext(conf, Seconds(1))
+
+    val lines = ssc.socketTextStream(args(0), args(1).toInt)
+    val words = lines.flatMap(_.split(" "))
+
+    val pairs = words.map(word => (word, 1))
+    val wordCounts = pairs.reduceByKey(_+_)
+
+    wordCounts.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
---
+++ spark-streaming/quick-example/build.sbt
@@ -0,0 +1,8 @@
+name := "spark-streaming-app"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.0.0"
+libraryDependencies += "org.apache.spark" %% "spark-streaming" % "2.0.0"
