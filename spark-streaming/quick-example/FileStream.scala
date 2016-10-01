d7dba7e7370adcfa68abe4addb9f6a318c721408
hugolu <printk@gmail.com>
2016-10-01 15:26:44 +0200

    add FileStreaming

---
+++ spark-streaming/quick-example/FileStream.scala
@@ -0,0 +1,19 @@
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+object FileStreaming {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("FileStreaming").setMaster("local[2]")
+    val ssc = new StreamingContext(conf, Seconds(5))
+
+    val lines = ssc.textFileStream("wordCount")
+    val words = lines.flatMap(_.split(" "))
+    val pairs = words.map(word => (word, 1))
+    val wordCounts = pairs.reduceByKey(_+_)
+
+    wordCounts.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
--- spark-streaming/quick-example/SocketStreaming.scala
+++ spark-streaming/quick-example/SocketStreaming.scala
@@ -1,21 +1,12 @@
-package org.apache.spark.examples.streaming
-
 import org.apache.spark.SparkConf
-import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
-object NetworkWordCount {
+object SocketStreaming {
   def main(args: Array[String]) {
-
-    if (args.length < 2) {
-      System.err.println("Usage: NetworkWordCount <hostname> <port>")
-      System.exit(1)
-    }
-
-    val conf = new SparkConf().setAppName("NetworkWordCount").setMaster("local[2]")
+    val conf = new SparkConf().setAppName("SocketStreaming").setMaster("local[2]")
     val ssc = new StreamingContext(conf, Seconds(5))
 
-    val lines = ssc.socketTextStream(args(0), args(1).toInt)
+    val lines = ssc.socketTextStream("localhost", 9999)
     val words = lines.flatMap(_.split(" "))
     val pairs = words.map(word => (word, 1))
     val wordCounts = pairs.reduceByKey(_+_)
