5f9ee3ea0db21a739bc409a54e23bb2478daaf71
hugolu <printk@gmail.com>
2016-10-04 13:35:06 +0200

    update the example of streaming window

--- spark-streaming/quick-example/WindowWordCount.scala
+++ spark-streaming/quick-example/WindowWordCount.scala
@@ -5,14 +5,14 @@ object WindowWordCount {
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("WindowWordCount").setMaster("local[4]")
     val sc = new SparkContext(conf)
-    val ssc = new StreamingContext(sc, Seconds(5))
+    val ssc = new StreamingContext(sc, Seconds(1))
     sc.setLogLevel("WARN")
     ssc.checkpoint(".")
 
     val lines = ssc.socketTextStream("localhost", 9999)
     val words = lines.flatMap(_.split(" "))
     val pairs = words.map(word => (word, 1))
-    val wordCounts = pairs.reduceByKeyAndWindow((a: Int, b: Int) => a + b, Seconds(30), Seconds(10))
+    val wordCounts = pairs.reduceByKeyAndWindow(_ + _, _ - _, Seconds(5), Seconds(2))
     wordCounts.print()
 
     ssc.start()
